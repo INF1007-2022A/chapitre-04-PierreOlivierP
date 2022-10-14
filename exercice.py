b809c85e3bc91ef5334cb9dd1fe621ad514ca4b6
PierreOlivierP <pierre-olivier.piquant@polymtl.ca>
2022-10-14 00:41:19 -0400

    damn

--- exercice.py
+++ exercice.py
@@ -17,19 +17,38 @@ def remove_third_char(string: str) -> str:
 
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
+    a = 0
+    for element in string:
+        if element == old_char:
+            newstring = string[0:a] + new_char + string[a+1:]
+        a +=1
+    return newstring
     pass
 
 
 def get_number_of_char(string: str, char: str) -> int:
-    i = 0
-    for charactere in ggs:
-        if charactere == what:
+    i = -1
+    for charactere in string:
+        if charactere == char:
             i = i+1
     return i
+
     pass
 
 
 def get_number_of_words(sentence: str, word: str) -> int:
+    i = 0
+    a = 0
+    b = 0
+    for element in sentence:
+        a = len(word)
+
+        if sentence[b:b+a] == word:
+            i +=1
+        if b+a == len(sentence):
+            break
+        b+=1
+    return i
     pass
 
 
